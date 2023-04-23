fddc7f7e13f8750cf67e22aee10e802e7b9ba2c2
Lou Barnes <8981024@qq.com>
2023-04-23 19:44:53 +0800

    移除 text completion

--- src/exe/install/install.sql
+++ src/exe/install/install.sql
@@ -1,34 +1,4 @@
 
-CREATE TABLE `openai_text_completion` (
-  `id` varchar(36) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'uuid()' COMMENT 'UUID',
-  `prompt` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '提问',
-  `lines` tinyint(4) NOT NULL DEFAULT '1' COMMENT '行数',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='文本应该会话';
-
-ALTER TABLE `openai_text_completion`
-ADD PRIMARY KEY (`id`);
-
-
-CREATE TABLE `openai_text_completion_message` (
-  `id` varchar(36) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'uuid()' COMMENT 'UUID',
-  `text_completion_id` varchar(36) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '会话ID',
-  `line` tinyint(4) NOT NULL DEFAULT '1' COMMENT '行号',
-  `prompt` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '提问',
-  `answer` text COLLATE utf8mb4_general_ci NOT NULL COMMENT '回答',
-  `times` tinyint(4) NOT NULL DEFAULT '0' COMMENT '失败重试次数',
-  `is_complete` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否完成',
-  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='文本应该会话记录';
-
-ALTER TABLE `openai_text_completion_message`
-ADD PRIMARY KEY (`id`),
-ADD KEY `text_completion_id` (`text_completion_id`);
-
-
-
 CREATE TABLE `openai_chat_completion` (
   `id` varchar(36) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'uuid()' COMMENT 'UUID',
   `prompt` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '提问',
--- src/exe/uninstall/uninstall.sql
+++ src/exe/uninstall/uninstall.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS `openai_text_completion`;
-DROP TABLE IF EXISTS `openai_text_completion_message`;
 DROP TABLE IF EXISTS `openai_chat_completion`;
 DROP TABLE IF EXISTS `openai_chat_completion_message`;
 DROP TABLE IF EXISTS `openai_image_generation`;
